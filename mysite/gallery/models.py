3764303dd5ec5ec9e2db11cdf8af4803367f7334
sensival <alstjs883@naver.com>
2024-11-08 01:38:35 +0900

    photo model에 게시자 추가

--- mysite/gallery/models.py
+++ mysite/gallery/models.py
@@ -8,6 +8,19 @@ class Patient(models.Model):
     name = models.CharField(max_length=100)
     ward = models.ForeignKey(Ward, on_delete=models.CASCADE)
 
+
+
+class Photo(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='photos')
+    photo = models.ImageField(upload_to='patient_photos/')
+    upload_time = models.DateTimeField(default=timezone.now)  # Default to current time
+    memo = models.TextField(blank=True, null=True)  # Optional memo
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uploaded_photos')  # 게시자
+
+    def __str__(self):
+        return f"Photo of {self.patient.name} - {self.upload_time}"
+
+'''
 class Photo(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='photos')
     photo = models.ImageField(upload_to='patient_photos/')
@@ -16,3 +29,4 @@ class Photo(models.Model):
 
     def __str__(self):
         return f"Photo of {self.patient.name} - {self.upload_time}"
+'''
