84aba982235440e1abc0ae990a296f245a8af9c1
sensival <alstjs883@naver.com>
2024-11-27 03:49:40 +0900

    root dir 설정

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home
 
 urlpatterns = [
+    path('', home, name='home'), 
     path('gallery/', include('gallery.urls')),
     path('admin/', admin.site.urls),
 ]
