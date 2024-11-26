7a2e61c18d3988656a32dc4dc1b1aa6cdc42b6c1
sensival <alstjs883@naver.com>
2024-11-27 03:56:17 +0900

    env 설정

--- mysite/frontend/src/env.js
+++ mysite/frontend/src/env.js
@@ -1,3 +1,3 @@
-export const REACT_APP_HOST_IP_ADDRESS = 'https://graduation-project-22a6.onrender.com';
+export const REACT_APP_HOST_IP_ADDRESS = 'https://graduation-project-22a6.onrender.com/';
 //export const REACT_APP_HOST_IP_ADDRESS = 'http://192.168.0.5:8000/';
 // 'http://localhost:8000/';
